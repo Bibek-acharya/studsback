182b7e15b375a0a04fa26370bbe36db23bdfa1cd
Durgesh-Bhandari <durgeshbhandari834@gmail.com>
2026-08-03 18:58:24 +0545

    feat: enhance email functionality by converting plain text to HTML format

--- internal/emailqueue/worker.go
+++ internal/emailqueue/worker.go
@@ -133,12 +133,12 @@ func sendEmail(to, subject, htmlBody string) error {
 	if strings.Contains(strings.ToLower(htmlBody), "<!doctype html>") || strings.Contains(strings.ToLower(htmlBody), "<html") {
 		fullBody = htmlBody
 	} else {
-		fullBody = renderEmailWrapper("Studsphere", htmlBody)
+		fullBody = renderEmailWrapper("Studsphere", plainTextToHTML(htmlBody))
 	}
 
 	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
-	msg := []byte(fmt.Sprintf("From: %s <%s>\nTo: %s\nSubject: %s\n%s%s",
-		fromName, smtpUser, to, subject, mime, fullBody))
+	msg := fmt.Appendf(nil, "From: %s <%s>\nTo: %s\nSubject: %s\n%s%s",
+		fromName, smtpUser, to, subject, mime, fullBody)
 
 	var auth smtp.Auth
 	if smtpUser != "" && smtpPass != "" {
@@ -438,6 +438,23 @@ func GetStudSphereLogoURL() string {
 	return "https://storage.studsphere.com/uploads/studsphere.png"
 }
 
+func plainTextToHTML(text string) string {
+	paragraphs := strings.Split(text, "\n\n")
+	var htmlParagraphs []string
+	for _, p := range paragraphs {
+		p = strings.TrimSpace(p)
+		if p == "" {
+			continue
+		}
+		escaped := strings.ReplaceAll(p, "&", "&amp;")
+		escaped = strings.ReplaceAll(escaped, "<", "&lt;")
+		escaped = strings.ReplaceAll(escaped, ">", "&gt;")
+		escaped = strings.ReplaceAll(escaped, "\n", "<br>")
+		htmlParagraphs = append(htmlParagraphs, "<p>"+escaped+"</p>")
+	}
+	return strings.Join(htmlParagraphs, "")
+}
+
 func renderEmailWrapper(title, content string) string {
 	return fmt.Sprintf(`
 <!DOCTYPE html>
@@ -514,10 +531,9 @@ func renderEmailWrapper(title, content string) string {
             text-decoration: none;
             color: #6b7280;
         }
-        .social-links svg {
+        .social-links img {
             width: 24px;
             height: 24px;
-            fill: currentColor;
         }
         .footer-text {
             color: #6b7280;
@@ -553,16 +569,16 @@ func renderEmailWrapper(title, content string) string {
         <div class="footer">
             <div class="social-links">
                 <a href="https://www.facebook.com/share/1CEcyRH9ZZ/" aria-label="Facebook">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95z"/></svg>
+                    <img src="https://cdn-icons-png.flaticon.com/512/5968/5968764.png" alt="Facebook" width="24" height="24">
                 </a>
                 <a href="https://www.instagram.com/stud.sphere?igsh=NDM5Z29nc2ZqMmc=" aria-label="Instagram">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2.16c3.2 0 3.58.01 4.85.07 3.25.15 4.77 1.69 4.92 4.92.06 1.27.07 1.65.07 4.85s-.01 3.58-.07 4.85c-.15 3.23-1.66 4.77-4.92 4.92-1.27.06-1.64.07-4.85.07s-3.58-.01-4.85-.07c-3.26-.15-4.77-1.7-4.92-4.92-.06-1.27-.07-1.64-.07-4.85s.01-3.58.07-4.85C2.38 3.85 3.9 2.31 7.15 2.23c1.27-.06 1.64-.07 4.85-.07m0-2.16C8.74 0 8.33.01 7.05.07c-4.26.19-6.78 2.71-6.98 6.98C0 8.33 0 8.74 0 12s.01 3.67.07 4.95c.2 4.27 2.72 6.79 6.98 6.98 1.28.06 1.69.07 4.95.07s3.67-.01 4.95-.07c4.27-.2 6.79-2.72 6.98-6.98.06-1.28.07-1.69.07-4.95s-.01-3.67-.07-4.95c-.2-4.27-2.72-6.79-6.98-6.98C15.67.01 15.26 0 12 0zm0 5.84A6.16 6.16 0 1 0 18.16 12 6.16 6.16 0 0 0 12 5.84zm0 10.16A4 4 0 1 1 16 12a4 4 0 0 1-4 4zm5.4-9.56a1.44 1.44 0 1 1-2.88 0 1.44 1.44 0 0 1 2.88 0z"/></svg>
+                    <img src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" alt="Instagram" width="24" height="24">
                 </a>
                 <a href="https://www.tiktok.com/@stud.sphere?_r=1&_t=ZS-95OYyC0vodM" aria-label="TikTok">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19.32 5.33a8.67 8.67 0 0 1-4.27-1.29v8.22a2.67 2.67 0 1 1-2.67-2.67c.14 0 .28 0 .41.05v-2.52a5.2 5.2 0 0 0-.41-.05 5.2 5.2 0 1 0 5.2 5.2V7.68a10.95 10.95 0 0 0 4.27.74V5.33z"/></svg>
+                    <img src="https://cdn-icons-png.flaticon.com/512/3046/3046121.png" alt="TikTok" width="24" height="24">
                 </a>
                 <a href="https://wa.me/9779800000000" aria-label="WhatsApp">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 0 1-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 0 1-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 0 1 2.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0 0 12.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 0 0 5.683 1.448h.005c6.554 0 11.890-5.335 11.893-11.893A11.821 11.821 0 0 0 20.885 3.488"/></svg>
+                    <img src="https://cdn-icons-png.flaticon.com/512/4462/4462466.png" alt="WhatsApp" width="24" height="24">
                 </a>
             </div>
             <div class="footer-text">
